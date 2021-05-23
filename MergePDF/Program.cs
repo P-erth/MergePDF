8748471b60373127e1b3b2173294c54167b29459
Juan Manuel Farizo <juanmanuelfarizo@gmail.com>
2021-05-23 18:41:02 -0300

    Merge pdf start

---
+++ MergePDF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.Drawing;
+using ZXing;
+
+namespace MergePDF
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string ruta= "sin ini";
+            foreach (string value in args) ruta = value;
+            Console.WriteLine(ruta);
+            Console.ReadLine();
+        }
+    }
+}
