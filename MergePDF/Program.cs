1668e98a3f6afd845fc8025f420881da5fd7d64e
P-ertho <ibisi@frro.utn.edu.ar>
2021-05-26 23:25:50 -0300

    .

--- MergePDF/Program.cs
+++ MergePDF/Program.cs
@@ -21,6 +21,7 @@ namespace MergePDF
         public static XFont fontCourierBold13 = new XFont("Courier New", 13, XFontStyle.Bold);
         public static XFont fontCourierBold7 = new XFont("Courier New", 7, XFontStyle.Bold);
         public static XFont fontCourier7 = new XFont("Courier New", 7, XFontStyle.Regular);
+        public static XFont fontCourier8 = new XFont("Courier New", 8, XFontStyle.Regular);
         public static XFont fontCourier6 = new XFont("Courier New", 6, XFontStyle.Regular);
         public static XFont fontCourierBold10 = new XFont("Courier New", 10, XFontStyle.Bold);
         public static XFont fontCourierBold9 = new XFont("Courier New", 9, XFontStyle.Bold);
@@ -245,19 +246,76 @@ namespace MergePDF
             recargos.Add(pagina.Substring(pivote += 40, 50));
             for (int i = 0; i < 11; i++) recargos.Add(pagina.Substring(pivote += 50, 50));
 
-
             String totControl = pagina.Substring(pivote += 50, 8);
             String total = int.Parse(pagina.Substring(pivote += 8, 10)).ToString() + "." + pagina.Substring(pivote += 10, 2);
-            Console.WriteLine("hasta aca hemos llegado");
 
+            String cod = pagina.Substring(pivote += 2, 25);
 
 
             ///desde aca se dibujan los textos
             int posy;
-            posy = 185;
+            posy = 197;
             //cuerpo
-            foreach (string cuerpo in cuerpos) gfx.DrawString(cuerpo, fontCourier7, XBrushes.Black, 243, posy += 7);
-            posy = 183;
+            foreach (string cuerpo in cuerpos) gfx.DrawString(cuerpo, fontCourier7, XBrushes.Black, 5, posy += 7);
+            posy = 197;
+            foreach (string cuerpo in cuerpos2) gfx.DrawString(cuerpo, fontCourier7, XBrushes.Black, 255, posy += 7);
+            posy = 490;
+            foreach (string deuda in deudas) gfx.DrawString(deuda, fontCourier7, XBrushes.Black, 189, posy += 7);
+            posy = 483;
+            foreach (string recargo in recargos) gfx.DrawString(recargo, fontCourier7, XBrushes.Black, 20, posy += 7);
+
+
+
+
+            gfx.DrawString("Numeración emitida como gran contribuyente el " + cuFecha + " a las " + cuHora , fontCourierBold7, XBrushes.Black, 20, 462);
+            gfx.DrawString(cuVto, fontCourierBold14, XBrushes.Black, 375, 491);
+            gfx.DrawString(total, fontCourierBold14, XBrushes.Black, 495, 491);
+
+            posy = 20;
+            gfx.DrawString("Liq. Serv. Publicos", fontCourierBold7, XBrushes.Black, 400, posy);
+            gfx.DrawString(lsp, fontCourierBold7, XBrushes.Black, 500, posy);
+            gfx.DrawString("Código Comprobante", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(lspCod, fontCourierBold7, XBrushes.Black, 555, posy);
+            gfx.DrawString("C.E.S.P Número", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(cespNum, fontCourierBold7, XBrushes.Black, 505, posy);
+            gfx.DrawString("Vencimiento CESP", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(cespVto, fontCourierBold7, XBrushes.Black, 522, posy);
+            gfx.DrawString("Control de pago", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(totControl, fontCourierBold7, XBrushes.Black, 530, posy);
+            gfx.DrawString("Fecha emisión", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(cespEmision, fontCourierBold7, XBrushes.Black, 522, posy);
+            gfx.DrawString("Estado Anter.", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(lspEstadoDF, fontCourierBold7, XBrushes.Black, 522, posy);
+            gfx.DrawString("Estado Actual", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(lspEstadoHF, fontCourierBold7, XBrushes.Black, 522, posy);
+            gfx.DrawString("Factor", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(int.Parse(lspFactor).ToString(), fontCourierBold7, XBrushes.Black, 556, posy);
+            gfx.DrawString("Coseno Fi.", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(lspCoseno, fontCourierBold7, XBrushes.Black, 548, posy);
+            gfx.DrawString("Secuencia", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(lspSecuencia, fontCourierBold7, XBrushes.Black, 523, posy);
+            gfx.DrawString("Tarifa", fontCourierBold7, XBrushes.Black, 400, posy += 10);
+            gfx.DrawString(lspTarifa, fontCourierBold7, XBrushes.Black, 527, posy);
+            gfx.DrawString("NIS", fontCourierBold14, XBrushes.Black, 397, posy += 15);
+            gfx.DrawString(int.Parse(nis).ToString(), fontCourierBold14, XBrushes.Black, 510, posy);
+
+
+            gfx.DrawString(nombre, fontCourierBold14, XBrushes.Black, 25, 92);
+            if (domiReal == localidad)
+            {
+                 gfx.DrawString(domiReal, fontCourierBold14, XBrushes.Black, 25, 105);
+            }
+            else
+            {
+                gfx.DrawString(domiReal, fontCourierBold14, XBrushes.Black, 25, 105);
+                gfx.DrawString(localidad, fontCourierBold14, XBrushes.Black, 25, 118);
+            }
+
+            gfx.DrawString("Cond.Iva: " + condiva, fontCourierBold7, XBrushes.Black, 25, 138);
+            gfx.DrawString("CUIT: " + cuit, fontCourierBold7, XBrushes.Black, 155, 138);
+            DrawBarCodeGrandesConsumos(gfx, cod);
+            gfx.DrawString("*" + cod + "*" , fontCourier6, XBrushes.Black, 70, 163);
+
         }
 
         private static void pdfGenerator(string pagina, PdfDocument document)
@@ -561,7 +619,7 @@ namespace MergePDF
 
         private static void DrawQR(XGraphics gfx, string qr1, string qr2)
         {
-            XImage img = XImage.FromFile("template2.jpg");
+            XImage img = XImage.FromFile("template.jpg");
             gfx.DrawImage(img, 0, 0);
 
             //Draw QR1
@@ -608,6 +666,27 @@ namespace MergePDF
             img = XImage.FromGdiPlusImage((Image)bm);
             img.Interpolate = false;
             gfx.DrawImage(img, 30, 578);
+        }
+
+        private static void DrawBarCodeGrandesConsumos(XGraphics gfx, string code)
+        {
+            var bcWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.CODE_128,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Height = 20,
+                    Width = 253,
+                    Margin = 0
+                },
+            };
+            bcWriter.Options.PureBarcode = true;
+            //Drawing BarCode1
+            Bitmap bm = bcWriter.Write(code);
+            XImage img = XImage.FromGdiPlusImage((Image)bm);
+            img.Interpolate = false;
+            gfx.DrawImage(img, 30, 142);
+            
         }
 
         static String CalculaCRC(String value, Encoding enc)
